9304507d1498a49b9c495b04e6579d1825ef5848
LeoPetry <leonardo.petry.br@gmail.com>
2014-02-02 13:53:26 -0500

    first draft

---
+++ config.rb
@@ -0,0 +1,8 @@
+# Require any additional compass plugins here.
+project_type = :stand_alone
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+output_style = :expanded
+relative_assets = true
+line_comments = false
